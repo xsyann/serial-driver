96c5324878a93b4262e200ed0a0e24a15a56c933
xsyann <ykoeth@xsyann.com>
2014-04-11 15:48:06 +0200

    Dynamic buffer allocation + Semaphore

--- serial_driver.c
+++ serial_driver.c
@@ -5,7 +5,7 @@
 ** Contact <contact@xsyann.com>
 **
 ** Started on  Wed Apr  9 14:07:16 2014 xsyann
-** Last update Fri Apr 11 12:21:27 2014 xsyann
+** Last update Fri Apr 11 15:47:20 2014 xsyann
 */
 
 #include <linux/kernel.h>
@@ -20,32 +20,62 @@
 #include "serial_driver.h"
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Nicolas de Thore, Yann KOETH");
-MODULE_DESCRIPTION("Simple serial driver for linux kernel v3.11.0.19");
-MODULE_VERSION("0.1");
-
-#define SD_DEVICE_NAME "serial-driver"
-
-#define SD_ERR_MAJOR "unable to get a major"
-#define SD_ERR_DEVADD "unable to add device"
-#define SD_ERR_DEVCREATE "unable to create device"
-
-#define SD_INFO_LOAD "loaded"
-#define SD_INFO_UNLOAD "unloaded"
-
+MODULE_AUTHOR(SD_AUTHOR);
+MODULE_DESCRIPTION(SD_DESC);
+MODULE_VERSION(SD_VERSION);
 
 static struct class *sd_class = NULL;   /* Device class (/sys/class) */
 static struct sd_dev *sd_dev = NULL;    /* Device wrapper */
 
+/* Data buffer dynamic allocation
+ * Called at first opening
+ */
+static int sd_alloc_data(struct sd_dev *dev)
+{
+        if (dev->data == NULL)
+        {
+                dev->data = kzalloc(SD_BUFFER_SIZE, GFP_KERNEL);
+                if (sd_dev->data == NULL)
+                {
+                        printk(KERN_WARNING "%s: open: %s",
+                               SD_DEVICE_NAME, SD_ERR_OUTOFMEM);
+                        return -ENOMEM;
+                }
+        }
+        return 0;
+}
 
 static int sd_open(struct inode *inode, struct file *filp)
 {
+        int err = 0;
+        unsigned int major = 0;
+        unsigned int minor = 0;
+
+        major = imajor(inode);
+        minor = iminor(inode);
+        /* Check minor and major */
+        if (MKDEV(major, minor) != sd_dev->devno ||
+            inode->i_cdev != &sd_dev->cdev)
+        {
+                printk(KERN_WARNING "%s: " SD_ERR_NOTFOUND "\n",
+                       SD_DEVICE_NAME, major, minor);
+                return -ENODEV;
+        }
+        /* Alloc data if data is NULL */
+        if (sd_dev->data == NULL)
+                if ((err = sd_alloc_data(sd_dev)) < 0)
+                        return err;
+        /* Lock */
+        if (down_interruptible(&sd_dev->sem) != 0)
+                return -EINTR;
         printk(KERN_INFO "%s: open()\n", SD_DEVICE_NAME);
         return 0;
 }
 
 static int sd_release(struct inode *inode, struct file *filp)
 {
+        /* Unlock */
+        up(&sd_dev->sem);
         printk(KERN_INFO "%s: release()\n", SD_DEVICE_NAME);
         return 0;
 }
@@ -89,6 +119,10 @@ static void sd_free_device(struct sd_dev *dev)
                 kfree(dev);
 }
 
+/* Add char device to system
+ * Call device_create() to auto create (udev daemon)
+ * the node in /dev.
+ */
 static int sd_create_device(struct sd_dev *dev, int major, int minor,
                             struct class *class)
 {
@@ -106,6 +140,7 @@ static int sd_create_device(struct sd_dev *dev, int major, int minor,
                        SD_DEVICE_NAME, err, SD_ERR_DEVADD);
                 return err;
         }
+
         device = device_create(class, NULL, dev->devno, NULL, SD_DEVICE_NAME);
         if (IS_ERR(device)) {
                 err = PTR_ERR(device);
@@ -135,7 +170,7 @@ static void sd_cleanup(void)
         unregister_chrdev_region(sd_dev->devno, 1);
 }
 
-/* Register char device range (dynamic major).
+/* Register char device (dynamic major).
  * Create device class.
  * The *major parameter is updated with major number.
  */
@@ -149,7 +184,9 @@ static int sd_setup(int *major)
                 printk(KERN_WARNING "%s: %s\n", SD_DEVICE_NAME, SD_ERR_MAJOR);
                 return err;
         }
+
         *major = MAJOR(first);
+
         sd_class = class_create(THIS_MODULE, SD_DEVICE_NAME);
         if (IS_ERR(sd_class)) {
                 err = PTR_ERR(sd_class);
@@ -168,7 +205,6 @@ static int __init sd_init_module(void)
 
         printk(KERN_INFO "%s: %s\n", SD_DEVICE_NAME, SD_INFO_LOAD);
         if ((err = sd_setup(&major))) {
-                sd_cleanup();
                 return err;
         }
         if ((err = sd_alloc_device(&sd_dev))) {
--- serial_driver.h
+++ serial_driver.h
@@ -5,11 +5,28 @@
 ** Contact <contact@xsyann.com>
 **
 ** Started on  Fri Apr 11 12:14:27 2014 xsyann
-** Last update Fri Apr 11 12:20:05 2014 xsyann
+** Last update Fri Apr 11 15:24:26 2014 xsyann
 */
 
 #ifndef         __SERIAL_DRIVER_H__
 #define         __SERIAL_DRIVER_H__
+
+#define SD_AUTHOR "Nicolas de Thore, Yann KOETH"
+#define SD_DESC "Simple serial driver for linux kernel v3.11.0.19"
+#define SD_VERSION "0.1"
+
+#define SD_DEVICE_NAME "serial-driver"
+
+#define SD_ERR_MAJOR "Unable to get a major"
+#define SD_ERR_DEVADD "Unable to add device"
+#define SD_ERR_DEVCREATE "Unable to create device"
+#define SD_ERR_NOTFOUND "Device %d:%d not found"
+#define SD_ERR_OUTOFMEM "Out of memory"
+
+#define SD_INFO_LOAD "Loaded"
+#define SD_INFO_UNLOAD "Unloaded"
+
+#define SD_BUFFER_SIZE 512
 
 struct sd_dev
 {
